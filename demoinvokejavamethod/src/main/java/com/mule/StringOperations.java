1d57908a70a50a452224f8fb5ca8061666d3e443
Ajay Kumar Nandamudi <anandamudi@corp.miraclesoft.com>
2021-01-21 18:37:16 +0530

    commit

---
+++ demoinvokejavamethod/src/main/java/com/mule/StringOperations.java
@@ -0,0 +1,17 @@
+package com.mule;
+
+public class StringOperations {
+	
+	// static method
+	public static String concat(String a, String b)
+	{
+		return a+" "+b;
+	}
+	
+	public String multiConcat(String a,String b,String c,String d)
+	{
+		return a+" "+b+" "+c+" "+d;
+	}
+
+}
+ 
